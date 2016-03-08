6f10eb96a7f0cef66f900da866f6105fd06b9929
Ignacio Palma <ignacio.palma.@live.com>
2016-03-08 01:26:23 -0300

    Setting up Bootstrap Gem with basic functionality like nav, sign-in and up forms with style

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get "about" => "pages#about"
   get "user_properties" => "pages#user_properties"
+  get "contact" => "pages#contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
